8ab374ecb59014b9a8a09341a336abaf6a6439dd
Thamar19 <2025-0039@unad.edu.do>
2025-03-19 21:34:08 -0400

    Ejercicio 1
    Escribe un programa que pida al usuario que ingrese un número entero positivo. El programa debe mostrar todos los números del 1 hasta el número ingresado, uno por uno, utilizando un bucle while.

---
+++ Ejercicio1.py
@@ -0,0 +1,14 @@
+#Bucle While #Bucle while
+numero = int(input("Ingrese un numero entero positivo:"))
+suma = 0
+i = 1
+
+while i <= numero:
+    print(i)
+    i += 1
+
+true = numero > 0
+if true:
+        print("El número es positivo entero")
+else:
+        print("El número es negativo entero")
